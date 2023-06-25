2f367e47ddef6ef873c46aa51ad979fc8a3ded67
Dan <thunderusnasdermn@yandex.ru>
2023-06-25 21:21:27 +0300

    feat: crash-test + удалил лишние файлы

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-const { errors } = require('celebrate');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const { errors } = require("celebrate");
+const cors = require("cors");
 
-const errorHandler = require('./middlewares/errorHandler');
-const router = require('./routes');
+const errorHandler = require("./middlewares/errorHandler");
+const router = require("./routes");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
@@ -15,7 +15,7 @@ const limiter = rateLimit({
   legacyHeaders: false,
 });
 
-mongoose.connect('mongodb://127.0.0.1/mestodb');
+mongoose.connect("mongodb://127.0.0.1/mestodb");
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -25,6 +25,11 @@ app.use(express.json());
 app.use(limiter);
 app.use(helmet());
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
 app.use(router);
 app.use(errors());
 app.use(errorHandler);
